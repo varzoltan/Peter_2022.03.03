fe1ee1e0419a2d224bc3e3d6d87590b0462d890a
varzoltan <64203391+varzoltan@users.noreply.github.com>
2022-03-10 16:26:03 +0100

    16:25

--- Peter_2022.03.03/Program.cs
+++ Peter_2022.03.03/Program.cs
@@ -9,28 +9,48 @@ namespace Peter_2022._03._03
 {
     internal class Program
     {
-        struct Adat
+        /*struct Adat
         {
             public int ora, perc, masodperc, halad_mp;
-            public string varos;     
-        }
-  
+            public string varos;
+        }*/
+        class Adat
+        {
+            public int ora{get;set;}
+            public int perc { get; set; }
+            public int masodperc { get; set; }
+            public int halad_mp { get; set; }
+            public string varos { get; set; }
+            public Adat(string sor)//konstruktor
+            {
+                string[] db = sor.Split(' ');
+                ora = int.Parse(db[0]);
+                perc = int.Parse(db[1]);
+                masodperc = int.Parse(db[2]);
+                halad_mp = int.Parse(db[3]);
+                varos = db[4];
+            }
 
+        }
+        
+        static List<Adat> lista = new List<Adat>();
+        //static Adat adatok = new Adat();
+        static void Beolvas()
+        {
+            int athalado_jarmu = int.Parse(File.ReadLines("forgalom.txt").First());
+            foreach (string i in File.ReadAllLines("forgalom.txt").Skip(1))
+            {        
+                lista.Add(new Adat(i));
+            }
+        }
         static void Main(string[] args)
         {
-            List<Adat> lista = new List<Adat>();
-            Adat adatok = new Adat();
-            int athalado_jarmu = int.Parse(File.ReadLines("forgalom.txt").First());
-            foreach(var i in File.ReadAllLines("forgalom.txt").Skip(1))
-            {
-                string[] db =i.Split(' ');
-                adatok.ora = int.Parse(db[0]);
-                adatok.perc = int.Parse(db[1]);
-                adatok.masodperc = int.Parse(db[2]);
-                adatok.halad_mp = int.Parse(db[3]);
-                adatok.varos = db[4];
-                lista.Add(adatok);
-            }
+            //1.feladat
+            Console.WriteLine("Adjon meg egy fájlnevet2");
+            string valami = Console.ReadLine();
+            Beolvas();
+
+            //2.feladat
         }
     }
 }
